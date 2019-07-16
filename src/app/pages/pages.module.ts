d6866e90f2a364c00d8aac544370a0000f4b8b63
Rafael <bsapphireprueba@gmail.com>
2019-07-16 09:57:42 -0500

    Modificaciones en los modulo

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+
+import { SharedModule } from '../shared/shared.module';
+
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesRoutingModule } from './pages.routes';
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PagesRoutingModule
+    ]
+})
+
+export class PagesModule{}
---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,31 @@
+import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+
+
+
+const routes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+    },
+
+
+];
+
+export const PagesRoutingModule = RouterModule.forChild(routes);
+
+// @NgModule({
+//     imports: [RouterModule.forChild(routes)],
+//     exports: [RouterModule]
+// })
+// export class PagesRoutingModule { }
