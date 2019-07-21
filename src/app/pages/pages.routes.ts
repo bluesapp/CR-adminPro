cac490e8f1efc26a23cd874b16b3bf02883db9b2
Rafael <bsapphireprueba@gmail.com>
2019-07-21 14:39:11 -0500

    Adicion de Breadcrumbs con titulos

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,7 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
-import { Graficas1Component } from "./graficas1/graficas1.component";
+import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
@@ -14,10 +14,10 @@ const routes: Routes = [
         path: '',
         component: PagesComponent,
         children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },
-            { path: 'account-settings', component: AccountSettingsComponent },
+            { path: 'dashboard', component: DashboardComponent, data: {titulo: 'Dahsboard'} },
+            { path: 'progress', component: ProgressComponent, data: {titulo: 'Progreso'}  },
+            { path: 'graficas1', component: Graficas1Component, data: {titulo: 'Graficas'}  },
+            { path: 'account-settings', component: AccountSettingsComponent, data: {titulo: 'Ajustes del tema'}  },
             { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
         ]
     },
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
+  titulo: string;
 
-  constructor() { }
+  constructor(private router: Router,
+              private title: Title) {
+
+    this.getDataRouter()
+      .subscribe(data => {
+        console.log(data);
+        this.titulo = data.titulo;
+        this.title.setTitle( this.titulo );
+      })
+
+  }
 
   ngOnInit() {
+  }
+
+  getDataRouter() {
+    return this.router.events.pipe(
+
+      filter(evento => evento instanceof ActivationEnd),
+      filter((evento: ActivationEnd) => evento.snapshot.firstChild === null),
+      map((evento: ActivationEnd) => evento.snapshot.data)
+
+    )
   }
 
 }
